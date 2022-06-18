89360aae06183b358c242ab0b48e728a8663f7fb
Mayur Kumar <mayurtheraj@googlemail.com>
2022-06-18 13:39:04 +0100

    Set up API Get request for 10 cats, created simple folder stucture of cat app

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,29 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const App = () => {
+  const [cats, setCats] = useState(null)
+
   useEffect(() => {
     const getData = async () => {
-      const { data } = await axios.get('/api/products/') // * <-- replace with your endpoint
+      const { data } = await axios.get('https://api.thecatapi.com/v1/images/search?limit=10&size=small')
       console.log(data)
+      setCats(data)
     }
     getData()
-  })
+  }, [])
 
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <h1>Cat v Cat</h1>
+      {cats && cats.map((cat, i) => {
+        return (
+          <img style={{ maxHeight: 200 }} src={cat.url} key={i} />
+        )
+      })}
+    </>
+  )
 }
 
 export default App
